88e01b86171b47674ea2cc51f612ad595f588802
Mohammed Khadadeh <mkhadadeh@scu.edu>
2019-02-06 12:52:05 -0800

    Started Motion of Asteroids

---
+++ Assets/asteroid_motion.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class asteroid_motion : MonoBehaviour {
+	Vector3 dir;
+	public float rot_force = 1000f;
+	// Use this for initialization
+	void Start () {
+		dir = new Vector3(Random.Range(0,10),Random.Range(0,10),Random.Range(0,10));
+	}
+
+	void FixedUpdate() {
+		float h = rot_force * Time.deltaTime;
+    float v = rot_force * Time.deltaTime;
+
+		gameObject.GetComponent<Rigidbody>().AddTorque(transform.up * h);
+    gameObject.GetComponent<Rigidbody>().AddTorque(transform.right * v);
+		gameObject.GetComponent<Rigidbody>().AddForce(dir * Time.deltaTime);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
