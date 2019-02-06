c6d35aec85bb884e68fbfee5a72b19869c19c542
Mohammed Khadadeh <mkhadadeh@scu.edu>
2019-02-06 15:26:04 -0800

    Motion of asteroids

--- Assets/asteroid_motion.cs
+++ Assets/asteroid_motion.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class asteroid_motion : MonoBehaviour {
 	Vector3 dir;
+	public float motion_vel = 100000000f;
 	public float rot_force = 1000f;
+	public float time_till_turn = 40f;
+	float timer;
+	bool enabled = false;
 	// Use this for initialization
 	void Start () {
-		dir = new Vector3(Random.Range(0,10),Random.Range(0,10),Random.Range(0,10));
+		dir = Vector3.Normalize(new Vector3(Random.Range(0,10),Random.Range(0,10),Random.Range(0,10)));
+		timer = time_till_turn;
 	}
-
 	void FixedUpdate() {
+		transform.position += (dir * motion_vel * Time.deltaTime);
 		float h = rot_force * Time.deltaTime;
     float v = rot_force * Time.deltaTime;
 
 		gameObject.GetComponent<Rigidbody>().AddTorque(transform.up * h);
     gameObject.GetComponent<Rigidbody>().AddTorque(transform.right * v);
-		gameObject.GetComponent<Rigidbody>().AddForce(dir * Time.deltaTime);
-	}
-
-	// Update is called once per frame
-	void Update () {
-
+		if(timer >= 0.0) {
+			timer -= Time.fixedDeltaTime;
+		} else {
+			timer = time_till_turn;
+			dir = -1 * dir;
+		}
 	}
 }
