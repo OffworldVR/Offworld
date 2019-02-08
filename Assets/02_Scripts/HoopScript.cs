f4783b0e89352d820c1b379ad4e7cc47b6651bcc
flamegiraffe <flamegiraffes@gmail.com>
2019-02-07 16:00:16 -0800

    new hoops, skybox, inner/outer hoop collision

---
+++ Assets/02_Scripts/HoopScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoopScript : MonoBehaviour {
+
+	public int hoopNum;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider c){
+		if(c.gameObject.tag=="Player"){
+			float d = (c.transform.position-transform.position).magnitude;
+			if(d<40){
+				if(d<25){
+					Debug.Log("hit inner");
+				}else{
+					Debug.Log("hit outer");
+				}
+			}
+		}
+	}
+}
