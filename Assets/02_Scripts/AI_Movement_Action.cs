d449678bcd7ffaec0d925a9d9039942b22474eed
Bilbo Baggins <scu.imaginarium@gmail.com>
2019-02-11 15:39:59 -0800

    Pluggable AI Scripts

---
+++ Assets/02_Scripts/AI_Movement_Action.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Actions/Move")]
+public class MoveAction : Action
+{
+    public GameObject MovementScript;
+
+    public override void Act(StateController controller)
+    {
+        Move(controller);
+    }
+
+    private void Move(StateController controller)
+    {
+        MovementScript.GetComponent<PlayerScript>().Move();
+    }
+}
