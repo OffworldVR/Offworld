07345a856ffeeb64be5e13c8af761f027ec7b090
Nat <natalie1vm@hotmail.com>
2019-04-29 14:57:02 -0700

    particle systems for all ship engines done

---
+++ Assets/Scripts/Spin_saucer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin_saucer : MonoBehaviour {
+	float rotateSpeed = -40f;
+	void Update () {
+			transform.RotateAround(transform.position, transform.up, rotateSpeed*Time.deltaTime);
+	}
+}
