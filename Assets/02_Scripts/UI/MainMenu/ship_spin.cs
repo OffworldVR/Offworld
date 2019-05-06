f8e44946498056ff53f83c7783245d28bc5ebdaf
Hagrid <scu.imaginarium@gmail.com>
2019-05-05 20:23:11 -0700

    Spinning ship models

---
+++ Assets/02_Scripts/UI/MainMenu/ship_spin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ship_spin : MonoBehaviour {
+
+    float tick;
+    float period;
+	// Use this for initialization
+	void Start () {
+        tick = 0f;
+        period = (2.0f * Mathf.PI) / 3.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.GetChild(0).Rotate(0, -1 * Mathf.Sin(period * tick), 0);
+        tick += Time.deltaTime;
+	}
+}
