2d41cf5fd9f229dcaf9175827d917714a9fe7b20
Hagrid <scu.imaginarium@gmail.com>
2019-05-09 15:49:53 -0700

    Added sprites and press any button to start text

---
+++ Assets/02_Scripts/UI/MainMenu/AnyButtonToStart_scr.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class AnyButtonToStart_scr : MonoBehaviour {
+
+    float alpha;
+    float time;
+    TextMeshProUGUI text;
+
+	// Use this for initialization
+	void Start () {
+        time = 0f;
+        text = GetComponent<TextMeshProUGUI>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        alpha = 0.25f * Mathf.Sin(4 * time) + 0.75f;
+        text.color = new Color32(255, 255, 255, (byte)((float)255 * alpha));
+	}
+}
